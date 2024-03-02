8024dc16c1770cd079eeaa768cd52e82030b9f5b
Yummy Fran Palomares <93894214+YummyFran@users.noreply.github.com>
2024-03-02 21:03:27 +0800

    added score and gameover

--- script.js
+++ script.js
@@ -3,32 +3,73 @@ const
     ctx = canvas.getContext('2d')
 
 let
-    sprite, trigger = false
+    sprite, scorer, animation,
+    trigger = false, stopper = false, gameOver = false
 
-const setSize = () => {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-}
+const 
+    setSize = () => {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+    },
 
-const init = () => {
-    setSize()
-    sprite = new Sprite(75)
-    sprite.draw()
-}
+    clearScreen = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+    },
 
-const animate = () => {
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    sprite.update()
-}
+    init = () => {
+        setSize()
+        trigger = false
+        stopper = false
+        gameOver = false
 
-const handleClick = e => {
-    if(!trigger){
-        trigger = true
-        animate()
+        sprite = new Sprite(75)
+        scorer = new Score()
+
+        sprite.draw()
+        scorer.draw()
+    }, 
+    
+    animate = () => {
+        clearScreen()
+        sprite.update()
+        scorer.draw()
+
+        if(gameOver) gameOverScreen()
+        else requestAnimationFrame(animate)
+    },
+
+    handleClick = e => {
+        if(!trigger){
+            trigger = true
+            animation = requestAnimationFrame(animate)
+        }
+
+        if(gameOver && stopper) {
+            clearScreen()
+            cancelAnimationFrame(animate)
+            init()
+        } else sprite.clicked(e.clientX, e.clientY)
+    },
+
+    gameOverScreen = () => {
+        clearScreen()
+        
+        ctx.fillStyle = '#2f2f2f'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+        ctx.fillStyle = '#fcfcfc'
+        ctx.fillText("Game Over", canvas.width / 2, canvas.height * 0.45)
+
+        ctx.font = '600 48px Arial'
+        ctx.fillText("Score:  " + scorer.value, canvas.width/2, canvas.height * 0.6)
+
+        setTimeout(() => {
+            ctx.font = '100 1rem Arial'
+            ctx.fillText("click anywhere to continue", canvas.width / 2, canvas.height * 0.9)
+
+            stopper = true
+        }, 1000)
     }
-    sprite.clicked(e.clientX, e.clientY)
-}
 
 class Sprite {
     constructor(r) {
@@ -43,6 +84,8 @@ class Sprite {
 
         this.y += this.dy
         this.draw()
+
+        if(this.y - this.r > canvas.height) gameOver = true
     }
 
     draw() {
@@ -60,11 +103,25 @@ class Sprite {
     clicked(x, y) {
         if(!this.isBound(x, y)) return
 
-        if(this.dy >= 0)
+        if(this.dy >= 0) {
             this.dy = -10
+            scorer.value++
+        }
     }
 }
 
+class Score {
+    constructor() {
+        this.value = 0
+    }
+
+    draw() {
+        ctx.font = '800 64px Arial'
+        ctx.textAlign = 'center'
+        ctx.fillStyle = '#AD88C6'
+        ctx.fillText(this.value, canvas.width / 2,  100)
+    }
+}
 
 
 
